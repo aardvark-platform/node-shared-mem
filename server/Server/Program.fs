677aaaaf7bfd8902cabfd04fc12079c72f32ddd3
krauthaufen <krauthaufen@awx.at>
2018-07-20 10:06:31 +0200

    added Server

---
+++ server/Server/Program.fs
@@ -0,0 +1,26 @@
+﻿open System
+open System.IO.MemoryMappedFiles
+open System.Runtime.InteropServices
+open System.Text
+open Microsoft.FSharp.NativeInterop
+
+#nowarn "9"
+
+[<EntryPoint>]
+let main argv =
+
+    use f = MemoryMappedFile.CreateNew("testfile", 1L <<< 20)
+    use v = f.CreateViewAccessor()
+    let ptr = v.SafeMemoryMappedViewHandle.DangerousGetHandle()
+
+    let arr = Encoding.UTF8.GetBytes "Hello From F#"
+    Marshal.Copy(arr, 0, ptr, arr.Length) 
+
+    while true do
+        printf "content# "
+        let line = System.Console.ReadLine()
+        let arr = Encoding.UTF8.GetBytes line
+        Marshal.Copy(arr, 0, ptr, arr.Length) 
+        NativePtr.write (NativePtr.ofNativeInt (ptr + nativeint arr.Length)) 0uy
+        
+    0
